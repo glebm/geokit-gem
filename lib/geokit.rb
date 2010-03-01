da9eb7ff16314d53ed4586a645e56eef2ba01f87
knapo <krzysztof.knapik@llp.pl>
2010-03-01 16:44:34 +0100

    Import acts_as_mappable

--- lib/geokit.rb
+++ lib/geokit.rb
@@ -26,7 +26,9 @@ $:.unshift path unless $:.include?(path)
 require 'geokit/geocoders'
 require 'geokit/mappable'
 require 'geokit/acts_as_mappable'
-ActiveRecord::Base.send :include, GeoKit::ActsAsMappable
 
 # make old-style module name "GeoKit" equivalent to new-style "Geokit"
 GeoKit=Geokit
+
+ActiveRecord::Base.send :include, GeoKit::ActsAsMappable
+
